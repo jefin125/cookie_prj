d6d4ee8d5196c676201ab567836ab807bf42015a
unknown <jefinprajan@gmail.com>
2023-06-02 12:14:59 +0530

    first commit

---
+++ myapp_cookie/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def setcookie(request):
+    response = HttpResponse("Cookie Set")
+    response.set_cookie('java-tutorial','javatpoint.com')
+    return response
+
+def getcookie(request):
+    tutorial = request.COOKIES['java-tutorial']
+    return HttpResponse("java tutorials@:"+ tutorial);
+
+# Create your views here.
